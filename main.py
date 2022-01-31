f757a3a3e0342010787cfacd0c5b8bb476dcb97f
INT\Huseyin.Aslan <huseyin.aslan@solera.com>
2022-01-31 15:38:45 +0100

    initial

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    print (request.headers)
+    return "Hello World!"
+
+@app.route("/best_song")
+def GetBestSongEver():
+    return "Never gonna give you up"
+
+if __name__ == "__main__":
+    app.run()
